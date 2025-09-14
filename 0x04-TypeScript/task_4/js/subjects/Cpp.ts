2b0106179c393de235d623dc7ed1ac1b3786f206
Brian Maleek <maleekmalik@gmail.com>
2025-09-14 19:31:16 +0300

    feat: add Cpp class with teacher interface and requirements methods

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,23 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+
+// Cpp.ts
+// Define the Cpp class within the Subjects namespace
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingC?: number;
+    }
+
+    export class Cpp extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Cpp";
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher && this.teacher.experienceTeachingC && this.teacher.experienceTeachingC > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+            return "No available teacher";
+        }
+    }
+}
