6dcf5d7b42e4e0a1f136c23e4369c7695668570c
Brian Maleek <maleekmalik@gmail.com>
2025-09-14 19:31:50 +0300

    feat: create Teacher interface within Subjects namespace

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+// Teacher.ts
+// Define the Teacher interface within the Subjects namespace
+namespace Subjects {
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+    }
+}
