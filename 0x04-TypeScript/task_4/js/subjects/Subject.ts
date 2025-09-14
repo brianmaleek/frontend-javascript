a8ace6bbd6451ac07e641b040d436e6a70380ead
Brian Maleek <maleekmalik@gmail.com>
2025-09-14 19:31:40 +0300

    feat: define Subject interface within Subjects namespace

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+// Subject.ts
+// Define the Subject interface within the Subjects namespace
+namespace Subjects {
+    export class Subject {
+        teacher: Teacher;
+
+        setTeacher(teacher: Teacher) {
+            this.teacher = teacher;
+        }
+    }
+}
